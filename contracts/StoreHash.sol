9b85781268628c60350af83f805206b6ac43d27d
Rahul Kothari <rahul.kothari.201@gmail.com>
2018-07-13 12:30:38 +0400

    changes on getting tasks

--- contracts/StoreHash.sol
+++ contracts/StoreHash.sol
@@ -4,19 +4,17 @@ contract StoreHash {
 
     uint task_id;  //person_id????????///???/////////?????//////??????
     mapping (uint => string) id_to_hashes;  
-    mapping(address => uint[]) parent_to_tasks;
+    mapping(uint => addres) task_to_parent;
     mapping(address => address[]) parent_to_children;
 
-    event HashAdded(uint id, address parent); //to store msg.sender and use it in front end!
-
-    //MODIFIER
+    //MODIFIERs=
     //onlyParent
     //onlyChildren can see the specific id modifier
     
     function addHash(string _ipfsHash) public {
         task_id++;  //use safemath.        
         id_to_hashes[task_id] = _ipfsHash;
-        emit HashAdded(task_id, msg.sender);
+        task_to_parent[task_id] = msg.sender;
     }
 
     function getLatestTaskId() public view returns (uint) {
--- react_ui/src/App.js
+++ react_ui/src/App.js
@@ -3,50 +3,89 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let table;
+    if(this.props.showTaskId) {
+      table = <table border = "1">
+        <tbody>
+          <tr>
+            <th>Task Detail Categories</th>
+            <th>Values </th>
+          </tr>
+          <tr>
+            <td>What is needed to be done:</td>
+            <td>{this.props.taskDesc}</td>
+          </tr>
+
+          <tr>
+            <td>Do the task by:</td>
+            <td>{this.props.deadline}</td>
+          </tr>
+
+          <tr>
+            <td>Reward (in finney)</td>
+            <td>{this.props.reward}</td>
+          </tr>
+
+          <tr>
+            <td>ipfs hash of the image (if available)</td>
+            <td>{this.props.imgIpfsHash}</td>
+          </tr>
+        </tbody>
+      </table>;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Add Details. Store in IPFS and in Ethereum!</h1>
         </header>
-
+        <br/>
+        <br/>
         <form onSubmit = { this.props.onSubmit }>
+          Describe the task:   
           <input
             type = "text"
+            name = "taskDesc"
             //value = {this.props.}
-            placeholder = "Describe the task"
-            onChange = { this.props.onDescribeChange}
+            placeholder = "Description"
+            //onChange = { this.props.onDescribeChange}
           />
 
           <br />
           <br />
 
-          <p>Enter the reward you are willing to give in FINNEY (1 ETHER = 100 FINNEY)  </p>
+          The reward you are willing to give in FINNEY (1 ETHER = 1000 FINNEY)
           <input
             type = "text"
+            name = "reward"
             //value = {this.props.}
             placeholder = "reward"
-            onChange = { this.props.onRewardChange}
+            //onChange = { this.props.onRewardChange}
           />
 
           <br />
           <br />
-
+          By when should the task be completed by: 
           <input 
             type = "text"
+            name = "deadline"
             //value
             placeholder = "deadline"
-            onChange = { this.props.onDeadlineChange}
+            //onChange = { this.props.onDeadlineChange}
           />
           
           <br/>
           <br/>
           
-          <p> If you want to add an image associated to your task: </p>
+          If you want to add an image associated to your task:
           <input
             type = "file"
             accept = "image/*"
-            onChange = { this.props.onImageChange}
+            onChange = { this.props.onImageChange }
           />
+
+          <br/>
+          <br/>
           
           <button type="submit" > Submit </button> 
         </form>
@@ -60,11 +99,32 @@ class App extends Component {
             </tr>
           
             <tr>
-              <td>IPFS Hash # stored on Eth Contract</td>
+              <td>Task id</td>
+              <td>{this.props.taskId}</td>
+            </tr>
+
+            <tr>
+              <td>IPFS Hash # of the task (also stored on Eth Contract)</td>
               <td>{this.props.ipfsHash}</td>
             </tr>
+
+            <tr>
+                <td>Ethereum Tx Hash # </td>
+                <td>{this.props.transactionHash}</td>
+              </tr>
           </tbody>
         </table>
+
+        <hr />
+        
+        <h4> Get task information by entering a task id </h4>
+        <form onSubmit = {this.props.onSubmitTaskId}>
+          <input type="text" placeholder="enter task id" name="task_id"/>
+          <input type="submit" />
+        </form>
+
+        {table}
+
       </div>
     );
   }
@@ -79,7 +139,39 @@ onDescribeChange
 onRewardChange
 onDeadlineChnage
 onImageChange
+onClick
+onSubmitTaskId
 
-gender
+taskId
+taskDesc
+reward
+deadline
 ipfsHash
+imgIpfsHash
+transactionHash
+showTaskId
+*/
+
+
+//DISPLAY THIS USING INFURA IPFS API on pressing getTransactionDetails BUTTON????????????!
+/*
+            <tr>
+              <td>IPFS Hash # of the task (also stored on Eth Contract)</td>
+              <td>{this.props.ipfsHash}</td>
+            </tr>
+
+            <tr>
+              <td>Tx Hash # </td>
+              <td>{this.props.transactionHash}</td>
+            </tr>
+
+            <tr>
+              <td>Block Number # </td>
+              <td>{this.props.blockNumber}</td>
+            </tr>
+
+            <tr>
+              <td>Gas Used</td>
+              <td>{this.props.gasUsed}</td>
+            </tr>
 */
